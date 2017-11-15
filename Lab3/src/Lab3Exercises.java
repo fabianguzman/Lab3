fa21d119271c29ebaffbefc69d5ba56618fd299a
4015d8 <4015d8@carambola.ece.uprm.edu>
2017-11-15 14:45:32 -0400

    Added a Panel Class

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,15 @@
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
+	
 	public static void main(String[] args) {
+		
 		JFrame myFrame = new JFrame("Fabian Omar");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(800, 400);
 		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
